38a67d38c019597f50f17245f81339bacc5e4940
Oscar Santamaria <osantamaria@gmail.com>
2020-04-22 21:59:26 -0600

    fix ci build failure

--- projects/mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderDto.java
+++ projects/mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderDto.java
@@ -32,10 +32,13 @@ import java.util.UUID;
 public class BeerOrderDto extends BaseItem {
 
     @Builder
-    public BeerOrderDto(UUID id, Integer version, OffsetDateTime createdDate, OffsetDateTime lastModifiedDate, UUID customerId, List<BeerOrderLineDto> beerOrderLines,
-                        OrderStatusEnum orderStatus, String orderStatusCallbackUrl, String customerRef) {
-        super(id, version, createdDate, lastModifiedDate);
-        this.customerId = customerId;
+    public BeerOrderDto(UUID id, Integer version, OffsetDateTime createdDate, OffsetDateTime lastModifiedDate, 
+    		UUID customerId, List<BeerOrderLineDto> beerOrderLines, String orderStatus, 
+    		String orderStatusCallbackUrl, String customerRef) {
+        
+    	super(id, version, createdDate, lastModifiedDate);
+        
+    	this.customerId = customerId;
         this.beerOrderLines = beerOrderLines;
         this.orderStatus = orderStatus;
         this.orderStatusCallbackUrl = orderStatusCallbackUrl;
@@ -45,6 +48,6 @@ public class BeerOrderDto extends BaseItem {
     private UUID customerId;
     private String customerRef;
     private List<BeerOrderLineDto> beerOrderLines;
-    private OrderStatusEnum orderStatus;
+    private String orderStatus;
     private String orderStatusCallbackUrl;
 }
