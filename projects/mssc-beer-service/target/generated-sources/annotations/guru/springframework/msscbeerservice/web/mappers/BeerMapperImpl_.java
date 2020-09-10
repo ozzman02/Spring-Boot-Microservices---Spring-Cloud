455c222dfece798c17e33d730fad262e6bca13a9
Oscar Santamaria <oscar.santamaria@janeirodigital.com>
2020-09-09 22:25:52 -0600

    Secure Spring Cloud Config Server

--- projects/mssc-beer-service/target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ projects/mssc-beer-service/target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-09-08T22:05:00-0600",
+    date = "2020-09-09T22:22:20-0600",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
 )
 @Component
