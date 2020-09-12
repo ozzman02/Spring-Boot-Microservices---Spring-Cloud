c9e8dddecb5731f00a7855bd7f67ee8e6121f870
Oscar Santamaria <oscar.santamaria@janeirodigital.com>
2020-09-11 23:13:15 -0600

    update bom and adding docker file

--- projects/mssc-beer-inventory-service/target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ projects/mssc-beer-inventory-service/target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-09-10T21:03:01-0600",
+    date = "2020-09-11T23:07:18-0600",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
 )
 @Component
